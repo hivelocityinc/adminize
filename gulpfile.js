9a58d3dc53a76fc5b6fc495573fc3d43ecffc60f
rayfields <raysbacker@gmail.com>
2015-12-03 23:50:27 +0900

    CSSのソースマップ対応

--- gulpfile.js
+++ gulpfile.js
@@ -3,53 +3,62 @@
 'use strict';
 
 // Node mobules
-var gulp            = require('gulp'),
-    sass            = require('gulp-sass'),
-    minifycss       = require('gulp-minify-css'),
-    rename          = require('gulp-rename'),
-    uglify          = require('gulp-uglify'),
-    plumber         = require('gulp-plumber'),
-    autoprefixer    = require('gulp-autoprefixer');
-
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
 
 // Config
 var config = {
-    style:  './src/scss/**/*.scss',
-    script: './src/js/**/*.js',
-    jade:   './test/jade/**/*.jade'
+  style:  './src/scss/**/*.scss',
+  script: './src/js/**/*.js',
+  jade:   './test/jade/**/*.jade'
 };
 
 
 gulp.task('styles', function () {
-    gulp.src([config.style])
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('css'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(minifycss())
-        .pipe(gulp.dest('css'));
+  return gulp.src('./src/scss/adminize.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass())
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe($.sourcemaps.write('.', {
+      includeContent: false,
+      sourceRoot: '.'
+    }))
+    .pipe(gulp.dest('css'))
+});
+
+gulp.task('minify', function () {
+  return gulp.src('./src/scss/adminize.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass())
+    .pipe($.cssnano())
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe($.sourcemaps.write('.', {
+      includeContent: false,
+      sourceRoot: '.'
+    }))
+    .pipe(gulp.dest('css'))
 });
 
 
 gulp.task('script', function () {
-    gulp.src(config.script)
-        .pipe(plumber())
-        .pipe(gulp.dest('js'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(uglify())
-        .pipe(gulp.dest('js'));
+  gulp.src(config.script)
+    .pipe($.plumber())
+    .pipe(gulp.dest('js'))
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe($.uglify())
+    .pipe(gulp.dest('js'));
 });
 
 
 gulp.task('watch', function () {
-    gulp.watch(config.style, ['styles']);
-    gulp.watch(config.script, ['script']);
+  gulp.watch(config.style, ['styles', 'minify']);
+  gulp.watch(config.script, ['script']);
 });
 
 
-gulp.task('serve', ['styles', 'script', 'watch']);
-gulp.task('default', ['watch']);
+gulp.task('default', ['styles', 'minify', 'script', 'watch']);
